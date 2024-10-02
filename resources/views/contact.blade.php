068318efea1984cffe741c221d7c4bb868ecfe3a
ArifkyWildan <muhammadarifkywildan@gmail.com>
2024-10-02 01:39:51 -0700

    commit baru

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,73 @@
+<x-app-layout>
+    <!doctype html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      @vite('resources/css/app.css')
+    </head>
+    <body>
+    <section class="bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
+          <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+              <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Contact</h2>
+              <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Halaman Ini Berisi Data Contact Siswa</p>
+          </div> 
+          <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+    
+            <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="image/hikam.JPG" alt="hikam.JPG">
+                  </a>
+                  <div class="p-5">
+                      <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          <a href="#">Hikam</a>
+                      </h3>
+                      <span class="text-gray-500 dark:text-gray-400">085894877489</span>
+                  </div>
+            </div> 
+    
+            <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="image/faras.JPG" alt="faras.JPG">
+                  </a>
+                  <div class="p-5">
+                      <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          <a href="#">Faras</a>
+                      </h3>
+                      <span class="text-gray-500 dark:text-gray-400">085894833489</span>
+                  </div>
+            </div> 
+    
+            <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="image/salman.JPG" alt="salman.JPG">
+                  </a>
+                  <div class="p-5">
+                      <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          <a href="#">Salman</a>
+                      </h3>
+                      <span class="text-gray-500 dark:text-gray-400">085856777489</span>
+                  </div>
+            </div> 
+    
+            <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+                  <a href="#">
+                      <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="image/geo.JPG" alt="geo.JPG">
+                  </a>
+                  <div class="p-5">
+                      <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          <a href="#">Geo</a>
+                      </h3>
+                      <span class="text-gray-500 dark:text-gray-400">085887677489</span>
+                  </div>
+            </div> 
+           
+           
+          </div>  
+      </div>
+    </section>
+    </body>
+    </html>
+    </x-app-layout>
+    
